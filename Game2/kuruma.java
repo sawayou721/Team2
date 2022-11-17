e890ca9bed8279dd78284d32891d25fb8180933a
Subaru-1023 <18082@hakodate.kosen-ac.jp>
2022-11-17 10:12:46 +0900

    Bがキャラ移動処理を追加

--- Game2/kuruma.java
+++ Game2/kuruma.java
@@ -15,5 +15,10 @@ public class kuruma extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) )
+        {
+            setRotation(0);
+            move(1);
+        }
     }    
 }
