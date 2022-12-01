41810f91383170cb7dbc933f57c0ea66767e0c88
sawayou721 <18034@hakodate.kosen-ac.jp>
2022-12-01 10:21:30 +0900

    プレイヤーを追加

---
+++ Game2/player.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class player extends Actor
+{
+    /**
+     * Act - do whatever the player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+                if( Greenfoot.isKeyDown( "right" ) )
+        {
+            setRotation(0);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-90);
+            move(1);
+        }// Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(1);
+ 
+        }// Add your action code here.
+    }    
+}
