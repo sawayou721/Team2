7421cae74b5d98a6d68064998e4bbe68ebd4652f
sawayou721 <18034@hakodate.kosen-ac.jp>
2022-12-15 09:37:49 +0900

    プレイヤー初期配置

--- Game2/MyWorld.java
+++ Game2/MyWorld.java
@@ -14,8 +14,9 @@ public class MyWorld extends World
      * 
      */
     public MyWorld()
-    {    
+    { 
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        addObject( new player(), 10, 50 );
     }
 }
